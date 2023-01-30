8a2219976245c703fc3e48c7fd9a6ff35177a984
philmui <thephilmui@gmail.com>
2023-01-30 15:05:18 -0800

    updating harvard to get up to 2001 data

--- run-harvard.sh
+++ run-harvard.sh
@@ -9,6 +9,6 @@
 # NOTE that Harvard opinion section has 3 types of articles
 #    f"https://www.thecrimson.com/tag/op-eds/page/", # (h1) 1-116, (h2) 117-190
 #    f"https://www.thecrimson.com/tag/editorials/page/", # (h1) 1-77, (h2) 78-124
-#    f"https://www.thecrimson.com/tag/columns/page/" #1-120
+#    f"https://www.thecrimson.com/tag/columns/page/" #1-120 ... 224
 
-unbuffer python ./src/harvard2parquet.py 1 120 1 2>&1 | tee -a ./data/harvard-opinion-LATEST.log
+unbuffer python ./src/harvard2parquet.py 120 224 1 2>&1 | tee -a ./data/harvard-opinion-LATEST.log
